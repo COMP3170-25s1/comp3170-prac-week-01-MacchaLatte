71bdfae3a0145573ca2337cb8f0c17e6163d6992
MacchaLatte <141289172+MacchaLatte@users.noreply.github.com>
2025-02-27 17:11:37 +1100

    .classpath added to week1

--- src/comp3170/week1/fragment.glsl
+++ src/comp3170/week1/fragment.glsl
@@ -1,18 +1,21 @@
 #version 410
 
-uniform vec3 u_colour;			// colour as a 3D vector (r,g,b)
-uniform vec2 u_screenSize;  	// screen dimensions in pixels
+uniform vec3 u_colour;   // Circle colour (r,g,b)
+uniform vec3 u_bgColour;       // Background colour (r,g,b)
+uniform vec2 u_screenSize;     // Screen dimensions in pixels
 
-layout(location = 0) out vec4 o_colour;	// output to colour buffer
+layout(location = 0) out vec4 o_colour;  // Output to colour buffer
 
 void main() {
-   vec2 p = gl_FragCoord.xy / u_screenSize;   // scale p into range (0,0) to (1,1)
-   float d = distance(p, vec2(0.5, 0.5));     // calculate distance to midpoint
-  
-   if (d < 0.5) {
-      o_colour = vec4(u_colour, 1);
+   vec2 p = gl_FragCoord.xy / u_screenSize;  // Normalize to (0,0) - (1,1)
+   vec2 circleCenter = vec2(0.4, 0.4);       // New circle center
+   float radius = 0.3;                       // New circle radius
+   float d = distance(p, circleCenter);      // Distance from new center
+
+   if (d < radius) {
+      o_colour = vec4(u_colour, 1);  // Circle color
    }
    else {
-      o_colour = vec4(0,0,0,1); // BLACK
+      o_colour = vec4(u_bgColour, 1);      // Background color
    }
 }
